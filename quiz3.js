2ccedcace1caa8e4d06a1863a6908b848c3c4bdb
keltonlee <keltonyt@gmail.com>
2021-05-03 22:37:17 +0800

    把quiz拉出

--- quiz3.js
+++ quiz3.js
@@ -1,14 +1,14 @@
 var dataUrl = "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a3e2b221-75e0-45c1-8f97-75acbd43d613";
 var data = $.getJSON(dataUrl);
-data.done(function(msg) {
-    $.each(msg.result.results, function(i, item) {
-        $("#contain").html();
-        $("#contain").append($("<h2/>").text(item.A_Name_Ch));
-        $("#contain").append($("<h3/>").text(item.A_Behavior));
-        $("#contain").append($("<img/>").attr("src", item.A_Pic04_URL));
-    });
+data.done( function( msg ) {
+  $.each(msg.result.results, function(i,item) {    
+    $("#contain").html();    
+    $("#contain").append($("<h2/>").text(item.A_Name_Ch));            
+    $("#contain").append($("<h3/>").text(item.A_Behavior));  
+    $("#contain").append($("<img/>").attr("src", item.A_Pic01_URL));  
+  });
 });
-data.fail(function(msg) {
+data.fail( function( msg ) {
     console.log(msg);
-    $("#contain").html("faiL getting data");
+    $("#contain").html("fail"); 
 });
